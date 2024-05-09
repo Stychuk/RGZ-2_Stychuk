7093f3a121cb3695d5f9383157eac78e2071c2ec
Stychuk <malinka27032004@gmail.com>
2024-05-09 21:12:19 +0300

    error

--- program_rgz2/src/program_rgz2.cpp
+++ program_rgz2/src/program_rgz2.cpp
@@ -7,9 +7,41 @@
 //============================================================================
 
 #include <iostream>
+#include <unordered_map>
+
 using namespace std;
 
+string shortenString(string str, int length) {
+    if (str.length() <= length) {
+        return str;
+    } else {
+        return str.substr(0, length - 3) + "...";
+    }
+}
+
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
-	return 0;
+    int size, length;
+    cout << "Введіть кількість рядків: ";
+    cin >> size;
+    cin.ignore();  // очищення символу нового рядка з буфера
+
+    string* strings = new string[size];
+    cout << "Введіть рядки:" << endl;
+    for (int i = 0; i < size; ++i) {
+        getline(cin, strings[i]);
+    }
+
+    cout << "Введіть довжину рядка: ";
+    cin >> length;
+
+    string* shortenedStrings = string[size];
+
+    for (int i = 0; i < size; ++i) {
+            shortenedStrings[i] = shortenString(strings[i], length);
+        }
+
+    delete[] strings;
+    delete[] shortenedStrings;
+
+    return 0;
 }
