297f3130feb7481fa9de0495cc18fc51d5871a61
Stychuk <malinka27032004@gmail.com>
2024-05-13 19:39:15 +0300

    Допрацьований код

--- program_rgz2/src/program_rgz2.cpp
+++ program_rgz2/src/program_rgz2.cpp
@@ -14,22 +14,21 @@ using namespace std;
 
 // Функція для скорочення рядка до заданої довжини
 string shortenString(const string& str, int length) {
-    if (length < 2) {
-        cout << "Неможливо скоротити рядок до довжини менше 2" << endl;
-        return str;
+    if (length <= 2) {
+        return "";
     }
     if (str.length() <= length) {
         return str;
     }
     // кількість крапок для скорочення
     string dots = "...";
-    if (length == 3) {
+    if (length == 4) {
         dots = "..";
-    } else if (length == 2) {
+    } else if (length == 3) {
         dots = ".";
     }
     // повернення скороченого рядка
-    return str.substr(0, length - dots.length()) + dots;
+    return str.substr(0, length - dots.length() - 1) + dots + str.back();
 }
 
 int main() {
@@ -40,21 +39,35 @@ int main() {
     cin >> size;
     cout << "Введіть довжину рядка для скорочення: ";
     cin >> length;
-    cin.ignore(); // очищення буфер вводу
-    for (int i = 0; i < size; ++i) {
-        string str;
-        cout << "Введіть рядок: ";
-        getline(cin, str);  // зчитування рядка з пробілами та іншими спеціальними символами
-        // скорочення рядка
-        string j = shortenString(str, length);
-        // якщо скорочений рядок вже існує, додаємо до нього номер
-        if (strings.count(j) > 0) {
-            strings[j]++;
-            j += to_string(strings[j]);
-        } else {
-            strings[j] = 1;
-        }
-        cout << "Скорочений рядок: " << j << endl;
+    if (length <= 2) {
+    	cout << "Неможливо скоротити рядок." << endl;
+    	system("pause");
+    	return 0;
     }
+    cin.ignore(); // очищення буфер вводу
+    string* inputStrings = new string[size]; // динамічний масив рядків
+    cout << "\nВведіть рядки:\n";
+    for (int i = 0; i < size; ++i) {
+    	getline(cin, inputStrings[i]); // заповнення масиву рядків
+    	}
+    cout << "\nСкорочені рядки: \n";
+    for (int i = 0; i < size; ++i) {
+    	// скорочення кожного рядка
+    	string j = shortenString(inputStrings[i], length);
+    	if (j == "") {
+    		cout << "Неможливо скоротити рядок." << endl;
+    		continue;
+    	}
+    	// якщо скорочений рядок вже існує, додаємо до нього номер
+    	if (strings.count(j) > 0) {
+    		strings[j]++;
+    		j = shortenString(j + to_string(strings[j]), length);
+    	} else {
+    		strings[j] = 1;
+    	}
+    	cout << j << endl;
+    }
+    delete[] inputStrings;
+    system("pause");
     return 0;
 }
